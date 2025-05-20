c536a828e85b60c80944106b99b04313aa2104df
PhuHuynh197 <huynhminhphu197@gmail.com>
2025-05-21 02:17:37 +0700

    [add] src test

---
+++ TestScansona/src/main/java/com/mycompany/testscansona/TestScansona.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testscansona;
+
+public class TestScansona {
+    public static void main(String[] args) {
+        System.out.println("Hello from ProjectSCGH2 - this is a clean scan!");
+    }
+}
+
